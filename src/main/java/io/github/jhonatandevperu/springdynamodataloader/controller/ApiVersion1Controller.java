9e7528c0664a0c409a29119520a28d0e79250241
Jhonatan <jcastsal@hotmail.com>
2023-05-15 23:33:04 -0500

    update server.servlet.context-path

--- src/main/java/io/github/jhonatandevperu/springdynamodataloader/controller/ApiVersion1Controller.java
+++ src/main/java/io/github/jhonatandevperu/springdynamodataloader/controller/ApiVersion1Controller.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 public class ApiVersion1Controller {
 
   private final DynamoDbService dynamoDbService;
